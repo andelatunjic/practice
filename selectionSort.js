24045aee9bf0a3bc9d179728a1de5479494856dc
andelatunjic <andtunjic@gmail.com>
2025-04-11 23:19:33 +0200

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Array<number>} arr The input integer array to be sorted.
+ * @return {Array<number>}
+ */
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex != i) [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  
+    return arr;
+  }
+  
+  export default selectionSort;
