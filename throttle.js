db3b9b27a988c201436aaba90ce6119d77f6bfdf
andelatunjic <andtunjic@gmail.com>
2025-04-13 17:17:38 +0200

    throttle

---
+++ throttle.js
@@ -0,0 +1,14 @@
+const throttle = (func, delay) => {
+  let isThrottled = false;
+
+  return (...args) => {
+    if (isThrottled) return;
+
+    func(...args);
+    isThrottled = true;
+    
+    setTimeout(() => {
+      isThrottled = false;
+    }, delay);
+  };
+};
